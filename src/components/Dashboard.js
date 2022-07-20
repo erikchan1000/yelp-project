a2d3011b568b0c522f2efb20814842d63c0659d0
Erik Chan <chanerik1000@gmail.com>
2022-07-20 13:20:08 -0700

    Logout Button Added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import NavBar from './NavBar'
+import { useAuth } from './Firebase/AuthContext'
+import {Link, useNavigate} from 'react-router-dom'
+import {useState} from 'react'
+import {Alert} from 'react-bootstrap'
+
 
 export default function Dashboard() {
+  const navigate = useNavigate()
+  const [error, setError] = useState(null)
+  const {currentUser, logout} = useAuth()
+
+  async function handleLogOut() {
+    setError(null)
+    
+    try {
+      await logout()
+      navigate('/')
+    }
+    catch (err) {
+      setError(err.message)
+    }
+  }
+
   return (
     <div>
         <NavBar/>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <button onClick={handleLogOut}>Log Out</button>
     </div>
   )
 }
