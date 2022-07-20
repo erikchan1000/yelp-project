cbd5de13e3f0c4818b66c05a684ee0744a956730
Erik Chan <chanerik1000@gmail.com>
2022-07-19 20:24:20 -0700

    Improved Login Page Animation

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,8 @@ import React, {useRef, useState} from 'react'
 import {Form, Button, Card, Alert} from 'react-bootstrap'
 import {useAuth} from './Firebase/AuthContext'
 import {Link, useNavigate} from 'react-router-dom'
+import './Login.css'
+
 
 export default function Login() {
     const emailRef = useRef();
@@ -29,30 +31,53 @@ export default function Login() {
 
   return (
     <>
-        <Card>
+        {/* <Card>
             <Card.Body>
                 <h2 className="text-center mb-4">Yelp Notifications Login</h2>
                 {error && <Alert variant="danger">{error}</Alert>}
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" ref={emailRef} required />
-                    </Form.Group>
+                <div className='login-form'>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required />
+                        </Form.Group>
 
-                    <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" ref={passwordRef} required />
-                    </Form.Group>
-                    <Button disabled={loading} className="w-100" type="submit">Login</Button>
-                </Form>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required />
+                        </Form.Group>
+                        <Button disabled={loading} className="w-100" type="submit">Login</Button>
+                    </Form>
+                </div>
                 <div className='w-100 text-center mt-3'>
                     <Link to='/forgot-password'>Forgot Password?</Link>
                 </div>
             </Card.Body>
-        </Card>
-        <div className="w-100 text-center mt-2">
-            Need an Account? <Link to="/signup">Sign Up</Link>
+        </Card> */}
+
+        <div class="login-box">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div class="user-box">
+                <input type="email" ref={emailRef} name="" required/>
+                <label>Username</label>
+                </div>
+                <div class="user-box">
+                <input type="password" ref={passwordRef} name="" required/>
+                <label>Password</label>
+                </div>
+                <button type="submit" class="btn">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    Login
+                </button>
+            </form>
         </div>
+        {/* <div className="w-100 text-center mt-2">
+            Need an Account? <Link to="/signup">Sign Up</Link>
+        </div> */}
     </>
   )
 }
