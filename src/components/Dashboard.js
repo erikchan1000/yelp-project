0af53bd2512298e2fa6ad8e22a4d3f2b2772b91b
Erik Chan <chanerik1000@gmail.com>
2022-07-19 18:49:08 -0700

    Implemented NavbarSkeleton

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+        <NavBar/>
+    </div>
   )
 }
