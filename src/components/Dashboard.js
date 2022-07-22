62786e77f798e158ae8244b8619e86b7da375e5f
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-07-22 12:42:28 -0400

    app.css needs work to make map look better, map pans to current location, map isn't rendering compass.svg correctly, need to add businesses to markers next

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,25 +7,39 @@ import { Alert } from "react-bootstrap";
 import Notifications from "./Notifications";
 import { onMessageListener } from "./Firebase/Firebase";
 import { Toast } from "react-bootstrap";
-import MapStyles from "../MapStyles";
+
+//imports for the map
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
 
+//constants for the map
 const libraries = ["places"];
 const mapContainerStyle = {
   width: "500px",
   height: "500px",
 };
+//NYC is hardcoded in for now
 const center = {
-  lat: 43,
-  lng: -79,
+  lat: 40.73061,
+  lng: -73.935242,
 };
 const options = {
-  styles: MapStyles,
+  //styles: MapStyles,
   disableDefaultUI: true,
   zoomControl: true,
 };
@@ -37,18 +51,44 @@ export default function Dashboard() {
   const [show, setShow] = useState(false);
   const [notification, setNotification] = useState({ title: "", body: "" });
 
-  {
-    /* take this out after project completion */
-  }
+  //take out key after project completion
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyCPt4cYU7eDJf5TKmAsLesZQsP9t9Kahjc",
     libraries,
   });
 
+  //have to set these to the api info, state to set map markers
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  //use this to prevent re-renders
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  //panTo
+  const panTo = React.useCallback(({ lat, lng }) => {
+    console.log(lat, lng);
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
 
-  console.log(show, notification);
+  //console.log(show, notification);
 
   onMessageListener()
     .then((payload) => {
@@ -105,7 +145,10 @@ export default function Dashboard() {
       )}
       <NavBar />
 
-      {/* Map displays here :) 
+      <Search panTo={panTo} />
+      <Locate panTo={panTo} />
+
+      {/* Map displays here
           Trying to figure out how to interface with the API,
           I think I'll need a search menu so people can look for what they want,
           then pan to the user's specified address and return the cuisines via the
@@ -115,10 +158,112 @@ export default function Dashboard() {
         zoom={8}
         center={center}
         options={options}
-      ></GoogleMap>
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {/* Need to add markers here */}
+        {markers.map((marker) => (
+          <Marker
+            key={`${marker.lat}-${marker.lng}`}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+
+        {/* business name here */}
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div></div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
 
       {error && <Alert variant="danger">{error}</Alert>}
       <button onClick={handleLogOut}>Log Out</button>
+    </div>
+  );
+}
+
+//returns the lat lng of the user's position
+function Locate({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+      <img src="../public/compass.svg" alt="compass" />
+    </button>
+  );
+}
+
+//DONE
+function Search({ panTo }) {
+  const {
+    ready, //to see if the script can load the values, handled by useLoad Script
+    value, //what is the current value the user has typed so far
+    suggestions: { status, data }, //suggestions from google's api
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 40.73061, lng: () => -73.935242 },
+      radius: 200 * 1000,
+    },
+  });
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  //DONE
+  return (
+    <div className="search">
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder="Search your location"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
     </div>
   );
 }
