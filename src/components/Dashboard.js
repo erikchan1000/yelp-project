d4b6f8e62bee645ded49a5a3c1c29dfa3f788b6a
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-07-21 22:13:34 -0400

    added some comments, taking a break

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,6 +7,7 @@ import { Alert } from "react-bootstrap";
 import Notifications from "./Notifications";
 import { onMessageListener } from "./Firebase/Firebase";
 import { Toast } from "react-bootstrap";
+import MapStyles from "../MapStyles";
 import {
   GoogleMap,
   useLoadScript,
@@ -16,12 +17,17 @@ import {
 
 const libraries = ["places"];
 const mapContainerStyle = {
-  width: "200px",
-  height: "200px",
+  width: "500px",
+  height: "500px",
 };
 const center = {
   lat: 43,
   lng: -79,
+};
+const options = {
+  styles: MapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
 };
 
 export default function Dashboard() {
@@ -31,6 +37,9 @@ export default function Dashboard() {
   const [show, setShow] = useState(false);
   const [notification, setNotification] = useState({ title: "", body: "" });
 
+  {
+    /* take this out after project completion */
+  }
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyCPt4cYU7eDJf5TKmAsLesZQsP9t9Kahjc",
     libraries,
@@ -96,10 +105,16 @@ export default function Dashboard() {
       )}
       <NavBar />
 
+      {/* Map displays here :) 
+          Trying to figure out how to interface with the API,
+          I think I'll need a search menu so people can look for what they want,
+          then pan to the user's specified address and return the cuisines via the
+          API */}
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={8}
         center={center}
+        options={options}
       ></GoogleMap>
 
       {error && <Alert variant="danger">{error}</Alert>}
