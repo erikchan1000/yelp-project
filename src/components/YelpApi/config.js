2de1deee91f037ba65e496604f21e336ca95f82b
erikchan1000 <chanerik1000@gmail.com>
2022-07-24 13:56:00 -0700

    Finished Beta Version

--- src/components/YelpApi/config.js
+++
@@ -1,5 +0,0 @@
-//I think something has to be prepended here for this to work
-//will test soon
-export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3'
-//DO NOT DO IN PRODUCTION
-export const BEARER_TOKEN = "V950eR70NgVzPbARgY8T_3gqktYLozGSgoaP0gp1a9mX1INGeu5Q_sb67ugokdmz4TOCzre3t4pdv4tQxIwwDchsNTzaBHIemM9xDL0vDW1oqnigZ3QuYf9sGtbYYnYx";
